f989a0634752a4afa4ac48512e0ee361339f16cd
Rafael Lyra <rafael@rafaellyra.com>
2015-01-24 23:29:48 -0200

    Install and set twig.js as default template engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var twig = require("twig");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,19 +10,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-/*
-var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
-*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'twig');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
