d3f0fa0143bd5dd236dd678ee6662badf08fcbd8
Rafael Lyra <rafael@rafaellyra.com>
2015-10-14 23:28:53 +0200

    Replace node-compass by stylus and fix bug on serving static files and change twig.js repository

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var stylus = require('stylus');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,11 +24,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
+
+//compiling stylus to css
+app.use(stylus.middleware({
+    src: __dirname + '/resources/stylesheets/',
+    dest: __dirname + '/public/stylesheets/',
+    debug: true,
+    force: true
+}));
+
+app.use(express.static(__dirname + '/public'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
